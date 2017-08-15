0a238b85c23befe1fd416dd7417709216fd64513
Giedrius Jonikas <giedriusj1@gmail.com>
2017-08-15 20:23:32 +0100

    +

--- agent.sh
+++ agent.sh
@@ -2,8 +2,8 @@ echo $HOSTNAME
 
 rm payload.*
 
-wget https://raw.githubusercontent.com/Giedriusj1/iot_command_and_control/master/systems/$HOSTNAME -O payload.enc
+wget https://raw.githubusercontent.com/Giedriusj1/iot_command_and_control/master/systems/$HOSTNAME.tar.gz.enc -O payload.enc
 
-gpg --output payload.sh --decrypt payload.enc
+gpg --output payload.tar.gz --decrypt payload.enc
 
-sh payload.sh
+# sh payload.sh
--- encrypt_payloads.sh
+++ encrypt_payloads.sh
@@ -1,1 +1,1 @@
-gpg --encrypt --output ./systems/black_arch --recipient "IoT control" ./systems/black_arch_plaintext
+tar -zcvf ./systems/black_arch.tar.gz  -C ./systems/black_arch . | gpg --encrypt --output ./systems/black_arch.tar.gz.enc --recipient "IoT control"
