32eb51cc6dc6f2987b809a3ed4079320d0947131
Giedrius Jonikas <giedriusj1@gmail.com>
2017-08-15 21:42:55 +0100

    +

--- agent.sh
+++ agent.sh
@@ -1,9 +1,15 @@
 echo $HOSTNAME
 
-rm payload.*
+rm $HOSTNAME.*
+rm -rf ./payload
 
-wget https://raw.githubusercontent.com/Giedriusj1/iot_command_and_control/master/systems/$HOSTNAME.tar.gz.enc -O payload.enc
+wget https://raw.githubusercontent.com/Giedriusj1/iot_command_and_control/master/systems/$HOSTNAME.tar.gz.enc
 
-gpg --output payload.tar.gz --decrypt payload.enc
+gpg --batch --yes --output payload.tar.gz --decrypt $HOSTNAME.tar.gz.enc
+
+
+mkdir ./payload
+
+tar -xf ./payload.tar.gz -C ./payload
 
 # sh payload.sh
