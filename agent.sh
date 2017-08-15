e249b009b9e7efe5d4d406761aacf5a45bb618bc
Giedrius Jonikas <giedriusj1@gmail.com>
2017-08-15 19:29:59 +0100

    +

--- agent.sh
+++ agent.sh
@@ -1,4 +1,8 @@
 echo $HOSTNAME
 
 
-wget https://raw.githubusercontent.com/Giedriusj1/iot_command_and_control/master/systems/$HOSTNAME
+wget https://raw.githubusercontent.com/Giedriusj1/iot_command_and_control/master/systems/$HOSTNAME payload.enc
+
+gpg --output $HOSTNAME.sh --decrypt $HOSTNAME
+
+sh $HOSTNAME.sh
