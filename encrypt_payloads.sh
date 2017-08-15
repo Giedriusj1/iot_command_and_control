ac2de397f617d4c5bad233a959d4f1f9875ad45b
Giedrius Jonikas <giedriusj1@gmail.com>
2017-08-15 20:28:55 +0100

    +

--- encrypt_payloads.sh
+++ encrypt_payloads.sh
@@ -1,1 +1,3 @@
-tar -zcvf ./systems/black_arch.tar.gz  -C ./systems/black_arch . | gpg --encrypt --output ./systems/black_arch.tar.gz.enc --recipient "IoT control"
+tar -zcvf ./systems/black_arch.tar.gz  -C ./systems/black_arch .
+
+gpg --encrypt --output ./systems/black_arch.tar.gz.enc --recipient "IoT control" ./systems/black_arch.tar.gz
